d167c6fb5bad9cd29fad4c22abd380a3da15ff52
Dan Durkin <ddurkin@broadinstitute.org>
2013-08-22 14:40:47 -0400

    added web client spring wiring to top level resources.groovy to help in trouble shooting

--- BARD/grails-app/conf/spring/resources.groovy
+++ BARD/grails-app/conf/spring/resources.groovy
@@ -1,9 +1,13 @@
 import acl.CapPermissionService
+import bard.core.rest.spring.DictionaryRestService
+import bard.core.util.ExternalUrlDTO
 import bard.db.ReadyForExtractFlushListener
 import bard.hibernate.ModifiedByListener
 import bard.person.RoleEditorRegistrar
+import bardqueryapi.QueryService
 import grails.util.Environment
 import org.codehaus.groovy.grails.orm.hibernate.HibernateEventListeners
+import org.springframework.web.client.RestTemplate
 
 // Place your Spring DSL code here
 beans = {
@@ -74,5 +78,36 @@ beans = {
     def extOntologyFactory = externalOntologyFactory(bard.validation.ext.RegisteringExternalOntologyFactory) { bean ->
         bean.factoryMethod = "getInstance"
     }
+
+    String ncgcBaseURL = grailsApplication.config.ncgc.server.root.url
+    String badApplePromiscuityUrl = grailsApplication.config.promiscuity.badapple.url
+    String bardCapUrl = grailsApplication.config.bard.cap.home
+
+    externalUrlDTO(ExternalUrlDTO) {
+        ncgcUrl = ncgcBaseURL
+        promiscuityUrl = badApplePromiscuityUrl
+        capUrl = bardCapUrl
+    }
+
+
+
+    restTemplate(RestTemplate)
+
+    dictionaryRestService(DictionaryRestService) {
+        externalUrlDTO = ref('externalUrlDTO')
+        restTemplate = ref('restTemplate')
+        loggerService = ref('loggerService')
+    }
+
+    queryService(QueryService) {
+        queryHelperService = ref('queryHelperService')
+        compoundRestService = ref('compoundRestService')
+        projectRestService = ref('projectRestService')
+        assayRestService = ref('assayRestService')
+        substanceRestService = ref('substanceRestService')
+        experimentRestService = ref('experimentRestService')
+        capRestService = ref('capRestService')
+    }
+
 
 }
